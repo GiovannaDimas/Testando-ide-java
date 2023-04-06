f45d5d9134866c45ed2442c1143843aad3b2683e
Giovanna Dimas <giovanna_nascdimas@outlook.com>
2023-04-06 02:17:45 -0300

    first commit

---
+++ src/br/com/dio/ProgramaJava.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.Teste_Gato;
+
+public class ProgramaJava {
+
+	public static void main(String[] args) {
+		
+		Teste_Gato Teste_Gato = new Teste_Gato();
+		System.out.println(Teste_Gato);
+		
+		/*importei o projeto Teste_Gato.java ainda com tudo null
+		 pois nao setei nada*/ 
+	}
+
+}
---
+++ src/ide/java/PrimeiroContatoIde.java
@@ -0,0 +1,15 @@
+package ide.java;
+
+public class PrimeiroContatoIde {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		System.out.println("Testando IDE Eclipse");
+		System.out.println("A soma dos valores A=2 e B=3 seria:");
+		System.out.println(a+b);
+
+	}
+
+}
